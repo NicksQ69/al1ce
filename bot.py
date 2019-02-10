c874e36a7454f1be6ed71fcdded3d32eb57a1bac
NicksQ69 <47458849+NicksQ69@users.noreply.github.com>
2019-02-10 02:01:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 TOKEN = 'Insert Token Here'
 
-description = '''Al1ce bot in Python'''
+description = '''AL1CE_Bot in Python'''
 bot = commands.Bot(command_prefix='.', description=description)
 
 @bot.event
@@ -23,10 +23,18 @@ async def hello():
 async def github():
     """Give github's link"""
     gitEmbed=discord.Embed(title="GitHub", url="https://github.com/NicksQ69/al1ce", description="GitHub by NicksQ69, co-written with Squidoss", color=0x0080ff)
-    gitEmbed.set_author(name="AL1CE",, icon_url="https://c.wallhere.com/photos/62/1e/shelter_video_Rin_Shelter_sky_space_planet-11546.jpg!d")
-    gitEmbed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1200px-Octicons-mark-github.svg.png")
+    gitEmbed.set_author(name="AL1CE", icon_url="https://i.imgur.com/TwxY5sr.png")
+    gitEmbed.set_thumbnail(url="https://i.imgur.com/IHRXykr.png")
     await bot.say(embed=gitEmbed)
     #await bot.say("https://github.com/NicksQ69/al1ce")
 
+@bot.command()
+async def website():
+    """Give official website's link"""
+    gitEmbed=discord.Embed(title="AL1CE", url="http://al1ce.fr", description="Official website of AL1CE, made by NicksQ69", color=0x0080ff)
+    gitEmbed.set_author(name="AL1CE", icon_url="https://i.imgur.com/TwxY5sr.png")
+    gitEmbed.set_thumbnail(url="https://i.imgur.com/Tyohpwv.png")
+    await bot.say(embed=gitEmbed)
+    #await bot.say("http://al1ce.fr")
 
 bot.run(TOKEN)
