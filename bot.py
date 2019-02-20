cc2a765416e67ef6bec4eeb542a86ef511b200a3
NicksQ69 <47458849+NicksQ69@users.noreply.github.com>
2019-02-20 11:21:04 +0100

    Update bot.py
    test add command "help"

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ TOKEN = file.read().rstrip("\n")        #Module discord, lecture du «TOKEN» co
 
 description = '''AL1CE_Bot in Python'''     #Description du bot.
 bot = commands.Bot(command_prefix='.', description=description)     #Définit le préfixe «.» pour ordonner le bot.
+bot.remove_command('help')
 
 @bot.event      #Démarrage du bot.
 async def on_ready():       #Définit la fonction de démarrage «on_ready».
@@ -21,6 +22,15 @@ async def on_ready():       #Définit la fonction de démarrage «on_ready».
     await bot.change_presence(game=discord.Game(name='you, master ... <3', type=2))     #Définit le statut du bot pour les utilisateurs «listening to».
 
 #Commands
+
+@bot.command(pass_context=True)
+async def help(ctx):
+    """Displays the commands"""
+    author = ctx.message.author
+    Embed = discord.Embed(color=0x0080ff)
+    Embed.set_author(name='Help')
+    Embed.add_field(name='.ping', inline=False)
+    await client.send_message(author, Embed=Embed)
 
 @bot.command()      #Définit une commande pour le bot.
 async def hello():      #Définit la fonction «hello».
@@ -59,5 +69,12 @@ async def reboot(ctx):      #Définit la fonction «reboot».
         sys.exit()      #Fermeture du programme «bot.py».
     else:       #Sinon, ...
         await bot.say("Unauthorized access")        #Afficher que l'accès n'est pas autorisé.
+        
+@bot.command()      #Définit une commande pour le bot.
+async def ping():       #Définit la fonction «ping».
+    """Get the latency"""       #Description de la commande «ping».
+    await bot.say("Pong!")      #Lecture de la commande par le bot.
+    
+
 
 bot.run(TOKEN)      #Exécution du bot à partir de la variable «TOKEN».
