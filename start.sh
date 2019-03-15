315a84a19efcace75b889df4dffca68e103cd81f
NicksQ69 <47458849+NicksQ69@users.noreply.github.com>
2019-03-15 23:56:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash   #Indication du répertoire de l'exécution d'un fichier type shell (bash).
+#!/bin/bash
 cd /home/al1ce/Bot/AL1CE    #Localisation de la racine du fichier «bot.py».
 git pull    #Téléchargements des modifications du programme situé sur github, pour l'appliquer sur le bot.
 python3 bot.py    #Exécution du programme sous python «bot.py».
