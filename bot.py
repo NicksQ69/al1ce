460d3d56cbb0534ffc59313b4f3c28a8af97955d
NicksQ69 <47458849+NicksQ69@users.noreply.github.com>
2019-03-17 14:27:48 +0100

    Update
    Adding pause, resume and stop

--- bot.py
+++ bot.py
@@ -101,4 +101,19 @@ async def play(ctx, url):
     players[server.id] = player
     player.start()
     
+@client.command(pass_context=True)
+async def pause(ctx):
+    id = ctx.message.server.id
+    players[id].pause()
+    
+@client.command(pass_context=True)
+async def stop(ctx):
+    id = ctx.message.server.id
+    players[id].stop()
+    
+@client.command(pass_context=True)
+async def resume(ctx):
+    id = ctx.message.server.id
+    players[id].resume()
+    
 client.run(TOKEN)      #Exécution du bot à partir de la variable «TOKEN».
