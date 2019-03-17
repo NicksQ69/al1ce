7b8b002f28f8def63870c8d8c832ea08151b8230
NicksQ69 <47458849+NicksQ69@users.noreply.github.com>
2019-03-17 14:54:05 +0100

    Update

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/al1ce/Bot/AL1CE    #Localisation de la racine du fichier «bot.py».
+cd /home/al1ce/Discord/al1ce    #Localisation de la racine du fichier «bot.py».
 git pull    #Téléchargements des modifications du programme situé sur github, pour l'appliquer sur le bot.
 python3 bot.py    #Exécution du programme sous python «bot.py».
